cc22eb4bfc4bb04f5ae26e8d018ba5d3b609cbae
Brian Sutton <brsutton@icloud.com>
2017-09-01 14:08:21 -0500

    assignments urls

--- src/urls.js
+++ src/urls.js
@@ -5,6 +5,9 @@ const urls = {
     USERS : "http://10.27.12.24:8080/users",
     //Assignmens
     ASSIGNMENTS : "http://10.27.12.24:8080/assignments",
+    ASSIGNMENTS_BY_ID : "http://10.27.12.24:8080/assignments/{id}",
+    ASSIGNMENTS_ASSIGN_BATCH : "http://10.27.12.24:8080/assignments/batch",
+	ASSIGNMENTS_ASSIGN_USER : "http://10.27.12.24:8080/assignments/user",
     //Batches
 
     //Submitions
