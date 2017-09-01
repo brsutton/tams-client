c5d5d4eae0ccea79aa29f9986fccba4340e04ec2
Brian Sutton <brsutton@icloud.com>
2017-09-01 14:04:40 -0500

    added users url

--- src/urls.js
+++ src/urls.js
@@ -2,7 +2,7 @@
 
 const urls = {
     //Users
-    USERS : "http://10.27.12.24:8080/assignments",
+    USERS : "http://10.27.12.24:8080/users",
     //Assignmens
     ASSIGNMENTS : "http://10.27.12.24:8080/assignments",
     //Batches
